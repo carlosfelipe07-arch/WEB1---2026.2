efa281456582c2cf8ea01a61d0077360af97e190
carlosfelipe07-arch <carlos.felipe07@aluno.ifce.edu.br>
2026-08-11 20:51:40 -0300

    Add files via upload

---
+++ WEB1 - 2026.2/EXEMPLOS/ex-07-28/main - teste index.js
@@ -0,0 +1,32 @@
+console.log("Hello, Word!");
+
+//console.log(idade);
+
+// criando blocos de codigo com chaves {}
+if (true) {
+    let teste = "hahahaha";
+    console.log(teste);
+}
+// let guarda escopo (bloco de codigo) e o var consegue acessar fora do bloco (chaves)
+
+
+let idade = 28;
+let nome = "Felipe";
+// variaveis criadas com let não são içadas(hoisting)
+console.log("A idade do individuo é: " + idade);
+
+// seleciona um elemento pelo seu ID
+document.getElementById("pnome").innerHTML =
+"O aluno " + nome + " tem " + idade + " anos de idade ";
+// o atributo defer é um boleano que ajuda a identificar a leitura de pagina. 
+
+nome = "chico da silva"
+
+const PI = 3.14;
+
+console.log(PI ** 3.14);
+// let consegue alterar valores e const mantem a constancia (NÃO MUDA)
+
+let ulitmo_nome; //Snake Case
+let primeiroNome; //Camel Case (Camelo)
+let PrimeiroNome; //Pascal Case
---
+++ WEB1 - 2026.2/EXEMPLOS/ex-08-04/main - teste index.js
@@ -0,0 +1,16 @@
+let idade = 16; //number
+let altura = 1.5; //number
+
+let autorizado = idade >= 15 && altura >1.4; // boolean
+
+console.log(autorizado);
+
+if (autorizado) {
+    console.log("Acesso Autorizado!");
+} else {
+    console.log("Acesso não autorizado!");
+}
+
+// Operador Términario
+/*autorizado ? console.log("Acesso autorizado!");
+*/
